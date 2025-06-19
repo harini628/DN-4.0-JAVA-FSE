2f46e1368321df989872690c10ece33a7732d199
Harini A <hari628122004@gmail.com>
2025-06-19 20:47:22 +0530

    Create Logger.java

---
+++ Implementing the Singleton Pattern/Logger.java
@@ -0,0 +1,15 @@
+package singletonpatternexample;
+
+public class Logger {
+    private static Logger instance;
+    private Logger(){
+        
+    }
+    public static Logger getInstance(){
+        if(instance == null){
+            instance = new Logger();
+        }
+        return instance;
+    }
+    
+}
