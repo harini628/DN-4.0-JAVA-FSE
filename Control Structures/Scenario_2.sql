665fed79c6c76a4754e029ffe8c668476d7879a3
Harini A <hari628122004@gmail.com>
2025-06-27 22:51:55 +0530

    Create Scenario_2.sql

---
+++ Control Structures/Scenario_2.sql
@@ -0,0 +1,13 @@
+--sql
+BEGIN
+    FOR cust IN (SELECT CustomerID FROM Customers WHERE Balance > 10000)
+    LOOP
+        UPDATE Customers
+        SET IsVIP = 'TRUE'
+        WHERE CustomerID = cust.CustomerID;
+    END LOOP;
+
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('VIP status updated for eligible customers.');
+END;
+/
