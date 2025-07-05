e321888b2acb72ddac99d50d013cb4fa806e517f
Harini A <hari628122004@gmail.com>
2025-07-05 14:51:29 +0530

    Create BookRepository.java

---
+++ WEEK_03/Exercise 1: Configuring a Basic Spring Application/CODE/com.library/com.library.repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+public class BookRepository {
+	public BookRepository() {
+		System.out.println("BookRepository is activated ");
+	}
+	public void Save() {
+		System.out.println("SAVED SUCCESSFULLY.....!");
+	}
+}
