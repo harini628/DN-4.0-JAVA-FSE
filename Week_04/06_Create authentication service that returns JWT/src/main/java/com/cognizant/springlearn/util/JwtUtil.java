0d83c299a6ebc920283af7f4d3fdeb8a58932b55
Harini A <hari628122004@gmail.com>
2025-07-12 02:41:57 +0530

    Create JwtUtil.java

---
+++ Week_04/06_Create authentication service that returns JWT/src/main/java/com/cognizant/springlearn/util/JwtUtil.java
@@ -0,0 +1,22 @@
+
+package com.cognizant.springlearn.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private String secret = "secretkey"; // Use a secure key in production
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10)) // 10 minutes
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
