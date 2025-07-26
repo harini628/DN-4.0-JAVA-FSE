23df4a9b7f6503933e31423105c5f0bbb549efc2
Harini A <hari628122004@gmail.com>
2025-07-26 15:47:14 +0530

    Add files via upload

---
+++ Week_6/2_ReactJS-HOL/Code/studentapp/src/Components/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Contact() {
+  return <h2>Welcome to the Contact Page of Student Management System</h2>;
+}
+
+export default Contact;
---
+++ Week_6/3_ReactJS-HOL/Code/studentapp/src/Components/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About() {
+  return <h2>Welcome to the About Page of Student Management System</h2>;
+}
+
+export default About;
